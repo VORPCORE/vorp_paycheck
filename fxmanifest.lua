65ecb241c8f1e7bca04154078b368bbb9601ba88
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-10-22 17:26:24 +0100

    new version 0.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,7 +13,7 @@ server_scripts {
     'config.lua'
 }
 
-version '0.1'
+version '0.2'
 vorp_checker 'yes'
 vorp_name '^4Resource version Check^3'
 vorp_github 'https://github.com/VORPCORE/vorp_paycheck'
